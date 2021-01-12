7f4fc9a4069c31bf2ea7b43cc4a96718943203f0
cniki006 <cniki006@edu.xamk.fi>
2021-01-12 11:38:40 +0200

    Made own folder, started on obstacle spawner

---
+++ Assets/Nikita/Scripts/ObstacleGenerationController_script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerationController_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject obstacle;
+    public float spawnRate;
+    public float gapSize;
+    public float gapSpawnBoundaries;
+    float Timer;
+    float gapCenter;
+    Vector2 spawnPoint;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gapCenter = Random.Range(0.0f, gapSpawnBoundaries);
+        spawnPoint.x = Screen.width / 2;
+        spawnPoint.y = gapCenter + gapSize / 2 + obstacle.transform.localScale.y;
+    }
+}
