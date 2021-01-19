025277c3795253e824397259383999e16af29061
cniki006 <cniki006@edu.xamk.fi>
2021-01-19 10:56:54 +0200

    Added pause function and game initialization.

---
+++ Assets/Scripts/GameManager_script.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float AIjumpMin, AIjumpMax;
+    public bool gameMenu;
+    public float jumpTimer = 0.0f;
+    GameObject obstacleSpawner;
+    GameObject player;
+    PlayerController player_script;
+
+    void Start()
+    {
+        player_script = GameObject.Find("Player").GetComponent<PlayerController>();
+        player = GameObject.Find("Player");
+        obstacleSpawner = GameObject.Find("ObstacleSpawner");
+        gameMenu = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameMenu == true)
+        {
+            obstacleSpawner.gameObject.SetActive(false);
+            AIjump();
+        } else
+        {
+            obstacleSpawner.gameObject.SetActive(true);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameMenu = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0; ;
+            }
+        }
+    }
+
+    void AIjump()
+    {
+        if (player.transform.position.y < -Camera.main.orthographicSize + Camera.main.orthographicSize / 2)
+        {
+            player_script.Jump();
+        }
+
+        if (player.transform.position.y > Camera.main.orthographicSize - Camera.main.orthographicSize / 2)
+        {
+            jumpTimer += 0.5f;
+        }
+        jumpTimer -= Time.deltaTime;
+        if (jumpTimer <= 0)
+        {
+            player_script.Jump();
+            jumpTimer = Random.Range(AIjumpMin, AIjumpMax);
+        }
+    }
+}
---
+++ Assets/Scripts/HUD_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HUD_script : MonoBehaviour
+{
+    GameManager_script gameManager_script;
+    Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager_script = GameObject.Find("GameManager").GetComponent<GameManager_script>();
+        canvas = this.gameObject.GetComponentInChildren<Canvas>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager_script.gameMenu == false)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
